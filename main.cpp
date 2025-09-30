f21acd6b11fdab20f3ff546075b74458a102a001
Abdullah Biswas <abdullahbiswasm@gmail.com>
2025-09-30 22:16:17 +0500

    Added a comment of things to do in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "HouseClass.cpp"
 
+//Working so far as far as I've checked
+//Have to remove the debugging lines and organize the code in a more cleaner way (more header and cpp files)
+//Also organize the output and implement the write to file function
+
 
 int main() {
     std::cout << "This is a program that analyzes your house plan and tells you what you need.\n"; 
